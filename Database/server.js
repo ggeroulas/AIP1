b80a5ae6c8f9273f068d7ee1379020e3bcc4e549
ggeroulas <georgy1@tpg.com.au>
2018-09-05 11:47:02 +1000

    some backend stuff, not working

--- Database/server.js
+++ Database/server.js
@@ -1,83 +1,10 @@
-const express = require('express');
-const app = express();
-
-const mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+//var body = require('body-parser'); Do we need this?
+var User = require('./user');
 mongoose.connect('mongodb://localhost:27017/temp', { useNewUrlParser: true });
 
-const User = mongoose.model('User', {
-    id: {type: Number, index: true},
-    name: [String],
-    password: [String],
-    score: [Number],
-    games: [
-        {
-            result: [Boolean],
-            dealer: [Number],
-            player: [Number]
-        }
-    ]
-});
-
-User
-    .remove({}) 
-    .then(() => {
-        new User({
-            id: 4592, 
-            name: 'Davy', 
-            password: 'doggo1',
-            score: 1000,
-            games: [
-                {
-                    result: true,
-                    dealer: 14,
-                    player: 17
-                }
-            ]
-        }).save();
-
-        new User({
-            id: 5284, 
-            name: 'Lisa', 
-            password: 'nootnoot',
-            score: 1000,
-            games: [
-                {
-                    result: true,
-                    dealer: 34,
-                    player: 11
-                }
-            ]
-        }).save();
-
-        new User({
-            id: 3729, 
-            name: 'Johnny', 
-            password: 'marksuccs1',
-            score: 1000,
-            games: [
-                {
-                    result: false,
-                    dealer: 12,
-                    player: 11
-                }
-            ]
-        }).save();
-        
-    });
-
-    new User({
-        id: 2999, 
-        name: 'Mark', 
-        password: 'whatastory',
-        score: 1000,
-        games: [
-            {
-                result: false,
-                dealer: 21,
-                player: 19
-            }
-        ]
-    }).save();
 
 app.use('/users', (req, res) => {
     User
@@ -94,7 +21,7 @@ app.use('/find', (req, res) => {
 app.use('/add', (req, res) => {
     console.log("hi");
     User
-        .insertMany({id: req.query.id, name: req.query.name, password: req.query.password, score: req.query.score})
+        .insertMany({name: req.query.name, password: req.query.password, score: req.query.score})
         .then(results => res.json(results));
         
 });
---
+++ Database/user.js
@@ -0,0 +1,65 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+// 
+var UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    passwordConf: {
+      type: String,
+      required: true,
+    },
+    score: {
+        type: Number,
+        required: true
+    },
+    games: [
+        {
+            result: [Boolean],
+            dealer: [Number],
+            player: [Number]
+        }
+    ]
+  });
+
+// Authentication function
+UserSchema.statics.authenticate = function (email, password, callback) {
+  User.findOne({ email: email })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+}
+
+//hashes the password
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
